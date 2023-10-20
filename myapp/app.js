c122121288bc3554a9d60ab181197dfb43fbde20
unknown <nohanj@seas.upenn.edu>
2023-10-20 16:51:04 -0400

    added basic routes

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const path = require('path')
+
 const port = 8080
 
 app.get('/', (req, res) => { 
@@ -8,16 +10,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login', (req, res) => { 
-    res.send('Login page here')
-    res.sendFile()
+    var options = {
+        root: path.join(__dirname, 'html-docs'),
+        dotfiles: 'deny'
+    }
+    var fileName = "login.html"
+    res.sendFile(fileName, options)
 })
 
-app.get('/lessons', (req, res) => { 
-    res.send('lessons')
+app.get('/lessons/:topic1/:topic2/:topic3', (req, res) => { 
+    res.send(req.params)
 })
 
-app.get('/questions-index', (req, res) => { 
-    res.send('questions index')
+app.get('/questions', (req, res) => { 
+    res.send('questions')
 })
 
 app.listen(port, () => {
