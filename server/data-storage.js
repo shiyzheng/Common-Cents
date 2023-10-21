82f322e7987c9e4dbb64680d6b5e0467737e3796
unknown <nohanj@seas.upenn.edu>
2023-10-21 02:46:26 -0400

    renamed directory to server

---
+++ server/data-storage.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testExport,
+  getLessons,
+};
+
+function testExport() {
+  console.log('function exported');
+}
+
+function getLessons() {
+  return ['Lesson1', 'Lesson2', 'Lesson3'];
+}
