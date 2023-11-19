c76688371fe065144a7bfbc1f8e371f8d882b0cf
unknown <nohanj@seas.upenn.edu>
2023-11-19 02:41:27 -0500

    adding server.js

--- server/routes.js
+++ server/routes.js
@@ -1,88 +1,99 @@
 import express from 'express'
 const app = express()
-const port = 8080
+// const port = 3000
 
 import pkg from 'body-parser';
 const { json } = pkg; 
 
-app.use(json())
-
-import { testExport, getLessons } from './data-storage.js'
-
-app.get('/', (req, res) => {
-    res.contentType('application/json')
-    res.send('Home page')
-    testExport()
-    res.status(200)
-});
-
-app.post('/', (req, res) => {
-    res.json(req.body)
-});
-
-
-
-app.get('/login', (req, res) => {
-     var options = {
-         root: ".",
-         dotfiles: 'deny'
-     }
-    var fileName = "./html-docs/login.html"
-    res.sendFile(fileName, options)
-});
-
-
-// retrieves a lesson
-app.get('/lessons', (req, res) => {
-    const lessons = getLessons();
-    res.json({ lessons: lessons });
-    const recursiveTest = {
-        test1 : "level1A",
-        test2 : "level1B",
-        r1 : {
-            test1 : "level2A",
-            test2: "level2B",
-            r1 : {
-                test1: "level3A",
-                test2: "level3B"
-            }
-        }
-    }
-    recursiveTraversal(recursiveTest) 
-})
-
-function recursiveTraversal(arg) {
-    for (const property in arg) {
-        if (typeof (arg[property]) == 'string') {
-            console.log("is string type: " + arg[property])
-        } else {
-            recursiveTraversal(arg[property])
-        }
-    }
+export {
+    home,
 }
 
+app.use(json())
+
+function home(req, res) {
+    console.log("home function called");
+    res.contentType('application/json');
+    res.send('Home page');
+    res.status(200);
+}
+
+// import { testExport, getLessons } from './data-storage.js'
+
+// app.get('/', (req, res) => {
+//     res.contentType('application/json')
+//     res.send('Home page')
+//     testExport()
+//     res.status(200)
+// });
+
+// app.post('/', (req, res) => {
+//     res.json(req.body)
+// });
 
 
-// adds a lesson
-app.post('/lessons', (req, res) => {
-    res.json(req.body);
-});
 
-// updates a lesson
-app.put('/lessons/:id', (req, res) => {
-//    const { id } = req.params;
-    res.json(req.body);
-});
+// app.get('/login', (req, res) => {
+//      var options = {
+//          root: ".",
+//          dotfiles: 'deny'
+//      }
+//     var fileName = "./html-docs/login.html"
+//     res.sendFile(fileName, options)
+// });
 
-app.delete('/lessons/:id', (req, res) => {
-    const { id } = req.params;
-    res.json({ deleted: id });
-});
 
-app.get('/lessons/:topic1/:topic2/:topic3', (req, res) => {
-    res.send(req.params)
-});
+// // retrieves a lesson
+// app.get('/lessons', (req, res) => {
+//     const lessons = getLessons();
+//     res.json({ lessons: lessons });
+//     const recursiveTest = {
+//         test1 : "level1A",
+//         test2 : "level1B",
+//         r1 : {
+//             test1 : "level2A",
+//             test2: "level2B",
+//             r1 : {
+//                 test1: "level3A",
+//                 test2: "level3B"
+//             }
+//         }
+//     }
+//     recursiveTraversal(recursiveTest) 
+// })
 
-app.listen(port, () => {
-    console.log(`Application listening on port ${port}`)
-}) 
+// function recursiveTraversal(arg) {
+//     for (const property in arg) {
+//         if (typeof (arg[property]) == 'string') {
+//             console.log("is string type: " + arg[property])
+//         } else {
+//             recursiveTraversal(arg[property])
+//         }
+//     }
+// }
+
+
+
+// // adds a lesson
+// app.post('/lessons', (req, res) => {
+//     res.json(req.body);
+// });
+
+// // updates a lesson
+// app.put('/lessons/:id', (req, res) => {
+// //    const { id } = req.params;
+//     res.json(req.body);
+// });
+
+// app.delete('/lessons/:id', (req, res) => {
+//     const { id } = req.params;
+//     res.json({ deleted: id });
+// });
+
+// app.get('/lessons/:topic1/:topic2/:topic3', (req, res) => {
+//     res.send(req.params)
+// });
+
+// app.listen(port, () => {
+//     console.log(`Application listening on port ${port}`)
+// }) 
---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+import * as routes from './routes.js'
+
+app.get('/', routes.home);
+
+app.listen(port, () => {
+    console.log(`Server Application listening on port ${port}`)
+})
