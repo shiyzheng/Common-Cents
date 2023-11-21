d44f62930a7db05edd2f694ca2a8d6fb73b54532
Anson Yan <ansonyan@seas.upenn.edu>
2023-11-21 01:56:29 -0500

    login style

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,6 +4,7 @@ import {
 } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from './Navbar';
+import '../styles/Login.css';
 
 function Login(props) {
   // signup page
@@ -37,41 +38,41 @@ function Login(props) {
   return (
     <div>
       <Navbar />
-      <form id="add" className="mx-auto" style={{ width: '800px' }}>
-        <h2>Login</h2>
-        <div className="form-group">
-          <label htmlFor="Username">
-            Username
-            <input type="text" className="form-control" id="Username" onChange={(e) => setUsername(e.target.value)} />
-          </label>
+      <div class="form-container">
+        <form id="add" className="mx-auto" style={{ width: '800px' }}>
+          <h2>Login</h2>
+          <div className="form-group">
+            <label htmlFor="Username">
+              <input size="40" type="text" placeholder="Email or username" className="form-control" id="Username" onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <br />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Password">
+              <input size="40" type="text" placeholder="Password" className="form-control" id="Password" onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <br />
+          </div>
+          <button
+            class="button big-btn"
+            data-testid="button"
+            onClick={(e) => {
+              e.preventDefault();
+              loginUser({ username, password });
+              const form = document.getElementById('add');
+              form.reset();
+            }}
+            type="submit"
+          >
+            LOG IN
+          </button>
           <br />
-        </div>
-        <div className="form-group">
-          <label htmlFor="Password">
-            Password
-            <input type="text" className="form-control" id="Password" onChange={(e) => setPassword(e.target.value)} />
-          </label>
-          <br />
-        </div>
-        <button
-          className="btn btn-primary"
-          data-testid="button"
-          onClick={(e) => {
-            e.preventDefault();
-            loginUser({ username, password });
-            const form = document.getElementById('add');
-            form.reset();
-          }}
-          type="submit"
-        >
-          Submit
-        </button>
-        <br />
-        <p>
-          {'Don\'t have an account? '}
-          <Link to="/signup">Signup here!</Link>
-        </p>
-      </form>
+          <p>
+            {'Don\'t have an account? '}
+            <Link to="/signup">Signup here!</Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
