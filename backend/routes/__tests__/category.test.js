535ea437d235f5b27cb8549392894854ad721758
unknown <nohanj@seas.upenn.edu>
2023-11-26 18:57:08 -0500

    update for category

--- backend/routes/__tests__/category.test.js
+++ backend/routes/__tests__/category.test.js
@@ -1,7 +1,14 @@
 const request = require('supertest');
 const { app } = require('../../server');
 
+const qs = require('qs')
+
 const STATUS = require('../category').STATUS;
+
+const { questionInCategory,
+        updateCategory,
+        getQuestion,
+} = require('../category');
 
 const PATH_PREFIX = "/category/"
 const NON_PATH = "doesNotExist";
@@ -10,7 +17,30 @@ const _TESTS = {
     BASE_NAMES: ["Test-Topic1", "Test-Topic2", "Test-Topic3"],
     INSERT_ONE: "Test-Inserted-Topic1",
     NO_OBJECT: {},
+
+    QUESTION_TOPIC: "Topic With Questions",
+    QUESTION: {
+        question: "Test Question Text?",
+        possibleAnswers: ["a1", "a2", "a3", "a4"],
+        correctAnswer: ["a3"],
+    }
 };
+
+// builds query params string given object of key value pairs
+// which can be appended after any valid url that contains ?
+function buildQueryParamsString(jsonObject) {
+    let res = "";
+    let first = true;
+    Object.keys(jsonObject).forEach(function (key) {
+        if (first) {
+            res += key + "=" + jsonObject[key];
+            first = false;
+        } else {
+            res += "&" + key + "=" + jsonObject[key];
+        }
+    });
+    return res;
+}
 
 
 describe('Routes Tests', function () {
@@ -60,12 +90,11 @@ describe('Routes Tests', function () {
 
         // deletes inserted topic if exist for some reason
         const res0 = await request(app).delete(`${PATH_PREFIX}${_TESTS.INSERT_ONE}`);
+
         const res1 = await request(app).put(`${PATH_PREFIX}${_TESTS.INSERT_ONE}`);
         const res2 = await request(app).get(`${PATH_PREFIX}${_TESTS.INSERT_ONE}`);
         const res3 = await request(app).delete(`${PATH_PREFIX}${_TESTS.INSERT_ONE}`);
         const res4 = await request(app).get(`${PATH_PREFIX}${_TESTS.INSERT_ONE}`);
-
-        console.log("res4.body:::", res4.body);
 
         // tests situation in which res0 deletes existing resource 
         // or the resource already does not exist
@@ -75,10 +104,66 @@ describe('Routes Tests', function () {
         expect(res2.statusCode).toBe(STATUS.OK); // resource exists
         expect(res3.statusCode).toBe(STATUS.OK); // resource deleted
         expect(res4.statusCode).toBe(STATUS.NOT_FOUND); // resource already deleted
+
         expect(res1.body.name).toEqual(`${_TESTS.INSERT_ONE}`)
         expect(res2.body.name).toEqual(`${_TESTS.INSERT_ONE}`)
         expect(res3.body.name).toEqual(`${_TESTS.INSERT_ONE}`)
         expect(res4.body).toEqual(_TESTS.NO_OBJECT)
     });
+
+    test('Tests question not in category and then in category', async () => {
+        let category = {
+            name: "category-name",
+            questions: []
+        }
+        const question = _TESTS.QUESTION;
+        expect(questionInCategory(category, question)).toBeFalsy();
+
+        category = updateCategory(category, question);
+        expect(questionInCategory(category, question)).toBeTruthy();
+    })
+
+    test('Tests getting a question from a url string', async () => {
+        const path_for_question = `${PATH_PREFIX}${_TESTS.QUESTION}`;
+        const queryParamsString = qs.stringify(_TESTS.QUESTION);
+        const queryParamsPath = `${path_for_question}?${queryParamsString}`;
+        const parsedQuestion = getQuestion(queryParamsPath);
+        expect(parsedQuestion).toEqual(_TESTS.QUESTION);
+    });
+
+    // test('Tests creating a category with query parameters', async () => {
+
+    //     const path_for_question = `${PATH_PREFIX}${_TESTS.QUESTION_TOPIC}`;
+
+    //     // deletes topic if exist for some reason
+    //     const res0 = await request(app).delete(path_for_question);
+
+    //     const queryParamsString = qs.stringify(_TESTS.QUESTION);
+    //     const queryParamsPath = `${path_for_question}?${queryParamsString}`;
+
+    //     const res1 = await request(app).put(queryParamsPath);
+    //     const res2 = await request(app).get(path_for_question);
+    //     const res3 = await request(app).delete(path_for_question);
+    //     const res4 = await request(app).get(path_for_question);
+
+    //     expect([204, 404]).toEqual(expect.arrayContaining([res0.statusCode]));
+
+    //     expect(res1.statusCode).toBe(STATUS.CREATED); // resource created
+    //     expect(res2.statusCode).toBe(STATUS.OK); // resource exists
+    //     expect(res3.statusCode).toBe(STATUS.OK); // resource deleted
+    //     expect(res4.statusCode).toBe(STATUS.NOT_FOUND); // resource not found 
+
+    //     // questions array was given one question, therefore should be size 1
+    //     expect(res1.body.questions.length).toBe(1);
+
+    //     // tests that correct question-topic object is returned from
+    //     // put, get, and delete operations by testing the question within the
+    //     // topic and that the question topic was successfully deleted.
+    //     expect(res1.body.questions[0]).toEqual(`${_TESTS.QUESTION}`)
+    //     expect(res2.body.questions[0]).toEqual(`${_TESTS.QUESTION}`)
+    //     expect(res3.body.questions[0]).toEqual(`${_TESTS.QUESTION}`)
+    //     expect(res4.body).toEqual(_TESTS.NO_OBJECT)
+
+    // });
 
 });
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const qs = require('qs')
+const _ = require('lodash');
+
 const Category = require('../models/category');
 
 const router = express.Router();
@@ -13,8 +16,62 @@ const STATUS = {
   NOT_FOUND: 404,
 }
 
+// specifies prefix of the query parameter name for questions
+const QUESTION_QUERY_PREFIX = "question";
+const QUESTION_NUMBERS = [1, 2, 3, 4, 5];
+
 function cloneObject(object) {
   return JSON.parse(JSON.stringify(object));
+}
+
+// given req.query, returns list of questions 
+// function extractQuestions(query) {
+//   let questionList = [];
+//   QUESTION_NUMBERS.forEach(element => {
+//     const queryName = QUESTION_QUERY_PREFIX + element; 
+//     if (query[queryName]) {
+//       questionList.push(query.queryName);
+//     }
+//   });
+//   console.log("questionList:::", questionList);
+//   return questionList;
+// }
+
+// returns true if the question is already in the category, else returns false
+function questionInCategory(category, question) {
+  let ret = false;
+  category.questions.forEach(element => {
+    console.log("element, question", element, question);
+    console.log(_.isEqual(element, question));
+    if (_.isEqual(element, question)) {
+      ret = true;
+    }
+  })
+  return ret;
+}
+
+// returns updated category with the question
+function updateCategory(category, question) {
+  category.questions.push(question);
+  return category;
+}
+
+// if the url encodes a question object, extracts it, else returns empty object
+function getQuestion(url) {
+  const tokens = url.split("?");
+  if (tokens.length < 2) {
+    return {};
+  } else {
+    return qs.parse(tokens[1]);
+  }
+}
+
+function buildEmptyCategory(name) {
+  const new_category = {
+    name: name,
+    questions: [],
+  };
+  return new_category;
 }
 
 router.get('/', async (req, res) => {
@@ -52,15 +109,12 @@ router.get('/:name', async (req, res) => {
   }
 });
 
+// used to create a new category if one does not exist yet
 router.put('/:name', async (req, res) => {
   try {
-    const { name } = req.params;
-    const new_category = {
-      name: name,
-      questions: []
-    }
-    const category = await Category.findOne({ name: name });
-    if (category == null) {
+    const existing_category = await Category.findOne({ name: req.params.name});
+    if (existing_category == null) {
+      const new_category = buildEmptyCategory(req.params.name); 
       Category.create(new_category);
       res.status(STATUS.CREATED);
       res.json(new_category);
@@ -96,5 +150,9 @@ router.delete('/:name', async (req, res) => {
 
 module.exports = {
   router,
-  STATUS: STATUS
+  STATUS: STATUS,
+
+  questionInCategory,  
+  updateCategory,
+  getQuestion,
 };
