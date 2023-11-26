ebe3882333dfa8d714b0588d96fb4eb97aff448d
unknown <nohanj@seas.upenn.edu>
2023-11-26 04:03:10 -0500

    get, put, delete operations for category names

--- backend/routes/__tests__/category.test.js
+++ backend/routes/__tests__/category.test.js
@@ -1,18 +1,82 @@
-const express = require('express');
 const request = require('supertest');
 const { app } = require('../../server');
 
-const TEST_BASE_NAMES = ["Test-Topic1", "Test-Topic2", "Test-Topic3"]
+const PATH_PREFIX = "/category/"
+const NON_PATH = "doesNotExist";
 
-describe('Routes Test', function () {
+const _TESTS = {
+    BASE_NAMES: ["Test-Topic1", "Test-Topic2", "Test-Topic3"],
+    INSERT_ONE: "Test-Inserted-Topic1",
+    NON_EXISTENT_OBJECT: {},
+};
+
+describe('Routes Tests', function () {
+
     test('Tests if received array contains all elements of expected array',
         async () => {
-            const res = await request(app).get('/category/');
+            const res = await request(app).get(`${PATH_PREFIX}`);
             expect(res.statusCode).toEqual(200);
-            names = []
+            const names = []
             res.body.forEach((element) => {
                 names.push(element.name);
             });
-            expect(names).toEqual(expect.arrayContaining(TEST_BASE_NAMES));
+            expect(names).toEqual(expect.arrayContaining(_TESTS.BASE_NAMES));
         });
+
+    test('Tests getting topic by name', async () => {
+            const index = 0;
+        const res =
+            await request(app).get(`${PATH_PREFIX}${_TESTS.BASE_NAMES[index]}`);
+            expect(res.statusCode).toEqual(200);
+            // test that correct topic is gotten 
+            expect(res.body.name).toEqual(_TESTS.BASE_NAMES[index]);
+    });
+
+    test('Tests getting non-existent topic', async () => {
+        const res = await request(app).get(`${PATH_PREFIX}${NON_PATH}`);
+        expect(res.statusCode).toEqual(404);
+        expect(res.body).toEqual(_TESTS.NON_EXISTENT_OBJECT);
+    });
+
+    test('Test inserting object that already exists', async () => {
+        const index = 0;
+        const res = await request(app).put(`${PATH_PREFIX}${_TESTS.BASE_NAMES[index]}`);
+        expect(res.statusCode).toEqual(409);
+        expect(res.body).toEqual(_TESTS.NON_EXISTENT_OBJECT);
+    });
+
+    test('Test deleting object that does not exist', async () => {
+        const res = await request(app).delete(`${PATH_PREFIX}${NON_PATH}`);
+        expect(res.statusCode).toEqual(404);
+        expect(res.body).toEqual(_TESTS.NON_EXISTENT_OBJECT);
+    });
+
+
+    test('Tests inserting then deleting a topic', async () => {
+
+        // deletes inserted topic if exist for some reason
+        const res0 = await request(app).delete(`${PATH_PREFIX}${_TESTS.INSERT_ONE}`);
+        const res1 = await request(app).put(`${PATH_PREFIX}${_TESTS.INSERT_ONE}`);
+        const res2 = await request(app).get(`${PATH_PREFIX}${_TESTS.INSERT_ONE}`);
+        const res3 = await request(app).delete(`${PATH_PREFIX}${_TESTS.INSERT_ONE}`);
+        const res4 = await request(app).get(`${PATH_PREFIX}${_TESTS.INSERT_ONE}`);
+
+        console.log("res4.body:::", res4.body);
+
+        // tests situation in which res0 deletes existing resource 
+        // or the resource already does not exist
+        expect([204, 404]).toEqual(expect.arrayContaining([res0.statusCode]));
+
+        expect(res1.statusCode).toBe(201); // resource created
+        expect(res2.statusCode).toBe(200); // resource exists
+        expect(res3.statusCode).toBe(200); // resource deleted
+        expect(res4.statusCode).toBe(404); // resource already deleted
+        console.log("sc1");
+        expect(res1.body.name).toEqual(`${_TESTS.INSERT_ONE}`)
+        expect(res2.body.name).toEqual(`${_TESTS.INSERT_ONE}`)
+        expect(res3.body.name).toEqual(`${_TESTS.INSERT_ONE}`)
+        expect(res4.body).toEqual(_TESTS.NON_EXISTENT_OBJECT)
+        console.log("sc2");
+    });
+
 });
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -4,6 +4,19 @@ const Category = require('../models/category');
 
 const router = express.Router();
 
+const NON_EXISTENT_OBJECT = {};
+
+const STATUS_CODES = {
+  OK: 200,
+  RESOURCE_CREATED: 201,
+  RESOURCE_EXISTS: 409,
+  RESOURCE_NOT_FOUND: 404,
+}
+
+function cloneObject(object) {
+  return JSON.parse(JSON.stringify(object));
+}
+
 router.get('/', async (req, res) => {
   try {
     const categories = await Category.find();
@@ -23,17 +36,66 @@ router.get('/getId', async (req, res) => {
   }
 });
 
-
-// admin insert, delete, modify operations for categories 
-router.put('/putname', async (req, res) => {
+router.get('/:name', async (req, res) => {
+  const { name } = req.params;
   try {
-    console.log("putname route reached");
-    const { name } = req.query;
+    const category = await Category.findOne({ name: name });
+    if (category != null) {
+      res.status(STATUS_CODES.OK);
+      res.json(category);
+    } else {
+      res.status(STATUS_CODES.RESOURCE_NOT_FOUND);
+      res.json(NON_EXISTENT_OBJECT);
+    }
+  } catch (e) {
+    res.send('error occurred: ' + e);
+  }
+});
+
+// admin insert, delete, modify operations for categories
+
+// put requests return the new resource that is created
+// or an empty object if the resource already exists
+// 409 status indicates resource already exists
+router.put('/:name', async (req, res) => {
+  try {
+    const { name } = req.params;
     const new_category = {
       name: name,
       questions: []
     }
-    Category.insertMany([new_category]);
+    const category = await Category.findOne({ name: name });
+    if (category == null) {
+      Category.create(new_category);
+      res.status(STATUS_CODES.RESOURCE_CREATED);
+      res.json(new_category);
+    } else {
+      res.status(STATUS_CODES.RESOURCE_EXISTS);
+      res.json({});
+    }
+  } catch (e) {
+    res.send('error occurred: ' + e);
+  }
+});
+
+// delete requests return the new resource that is created 
+// or an empty object if the resource does not exist
+router.delete('/:name', async (req, res) => {
+  try {
+    const { name } = req.params;
+    const category = await Category.findOne({ name: name });
+    const clone = await cloneObject(category);
+    if (category != null) {
+      await Category.deleteOne({ name: name });
+      console.log("category from delete:::", category);
+      console.log("category from cloning:::", clone);
+      res.status(STATUS_CODES.OK);
+      res.type('application/json');
+      res.json(clone);
+    } else {
+      res.status(STATUS_CODES.RESOURCE_NOT_FOUND);
+      res.json({});
+    }
   } catch (e) {
     res.send('error occurred: ' + e);
   }
