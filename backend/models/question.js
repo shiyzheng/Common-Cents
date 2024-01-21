0f461d2c8423b4c7de524e04b2646631ee1d6af1
unknown <nohanj@seas.upenn.edu>
2024-01-21 15:00:04 -0500

    adding question collection

---
+++ backend/models/question.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const questionSchema = new Schema({
+    question: { type: String, required: true },
+    possibleAnswers: { type: [String], required: true },
+    correctAnswer: {type: String, required: true },
+})
+
+const Question = model('Question', questionSchema);
+
+module.exports = Question;
