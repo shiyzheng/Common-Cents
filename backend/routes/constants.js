5c01d7d3ba701e9b3d496933546d7b8b9da1b585
unknown <nohanj@seas.upenn.edu>
2024-01-21 19:29:48 -0500

    added constants file

---
+++ backend/routes/constants.js
@@ -0,0 +1,15 @@
+// file contains common constants use in routes
+
+const NO_OBJECT = {};
+
+const STATUS = {
+  OK: 200,
+  CREATED: 201,
+  EXISTS: 409,
+  NOT_FOUND: 404,
+}
+
+module.exports = {
+    STATUS: STATUS,
+    NO_OBJECT
+};
