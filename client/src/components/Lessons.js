170f70e03650c57930bdbbc35cad71de13c0b854
shiyzheng <syz@seas.upenn.edu>
2024-01-21 20:58:29 -0500

    redo lesson

--- client/src/components/Lessons.js
+++ client/src/components/Lessons.js
@@ -1,18 +1,81 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import '../styles/Lessons.css';
 
 const Lessons = (props) => {
   const {
     login, username, setUsername, setLogin
     } = props;
+  const navigate = useNavigate();
   const { topic } = useParams();
   const formattedTopic = topic.replace(/-/g, ' ').replace(/(?:-| |\b)(\w)/g, function(key) { return key.toUpperCase()});
 
+  const lessonList = [
+    'Lesson 1',
+    'Lesson 2',
+    'Lesson 3',
+  ];
+
+  const lessonList2 = [
+    'Lesson 1',
+    'Lesson 2',
+    'Lesson 3',
+    'Lesson 4'];
+
+  const levelsList = [
+    'Level 1', 
+    'Level 2', 
+    'Level 3',
+    'Level 4'];
+
+    const renderLessonLinks = (level) => {
+      const formattedLevel = level.toLowerCase().replace(/\s+/g, '-');
+      return lessonList.map((lesson, index) => (
+        <li key={index}>
+          <button class="button" onClick={() => navigate(`/lessons/${encodeURIComponent(topic)}/${formattedLevel}/${index + 1}`)}>
+            {`${lesson}`}
+          </button>
+          <h6>{" "}</h6>
+        </li>
+      ));
+    };
+
+    const renderLessonLinks2 = (level) => {
+      const formattedLevel = level.toLowerCase().replace(/\s+/g, '-');
+      return lessonList2.map((lesson, index) => (
+        <li key={index}>
+          <button class="button" onClick={() => navigate(`/lessons/${encodeURIComponent(topic)}/${formattedLevel}/${index + 1}`)}>
+            {`${lesson}`}
+          </button>
+          <h6>{" "}</h6>
+        </li>
+      ));
+    };
+
   return (
     <><Navbar setLogin={setLogin} login={login} setUsername={setUsername} username={username} /><div>
-          <h2>{formattedTopic}</h2>
-          Placeholder text for lessons.
+          <div className="lessons-container">
+      <h2>{formattedTopic}</h2>
+      <div className="lessons-wrapper">
+
+      <div className="level-container">
+        <h3>Level 1</h3>
+        <ul>{renderLessonLinks('Level 1')}</ul>
+      </div>
+          
+      <div className="level-container">
+        <h3>Level 2</h3>
+        <ul>{renderLessonLinks('Level 2')}</ul>
+      </div>
+
+      <div className="level-container">
+        <h3>Level 3</h3>
+        <ul>{renderLessonLinks2('Level 3')}</ul>
+      </div>
+
+      </div>
+    </div>
       </div></>
   );
 };
