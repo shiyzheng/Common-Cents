282332ce0b3945d8a0a0910c06740475263dbf78
shiyzheng <syz@seas.upenn.edu>
2024-03-17 22:30:06 -0400

    fix leaderboards button

--- client/src/pages/Leaderboards.js
+++ client/src/pages/Leaderboards.js
@@ -9,30 +9,6 @@ function Leaderboards(props) {
   } = props;
     const usersPerPage = 10;
     const [currentPage, setCurrentPage] = useState(1);
-    // const users = [
-    //     { id: 1, name: "User 1", points: 120 },
-    //     { id: 2, name: "User 2", points: 90 },
-    //     { id: 3, name: "User 3", points: 150 },
-    //     { id: 4, name: "User 4", points: 75 },
-    //     { id: 5, name: "User 5", points: 200 },
-    //     { id: 6, name: "User 6", points: 110 },
-    //     { id: 7, name: "User 7", points: 180 },
-    //     { id: 8, name: "User 8", points: 95 },
-    //     { id: 9, name: "User 9", points: 130 },
-    //     { id: 10, name: "User 10", points: 160 },
-    //     { id: 11, name: "User 11", points: 85 },
-    //     { id: 12, name: "User 12", points: 105 },
-    //     { id: 13, name: "User 13", points: 140 },
-    //     { id: 14, name: "User 14", points: 70 },
-    //     { id: 15, name: "User 15", points: 180 },
-    //     { id: 16, name: "User 16", points: 200 },
-    //     { id: 17, name: "User 17", points: 110 },
-    //     { id: 18, name: "User 18", points: 95 },
-    //     { id: 19, name: "User 19", points: 120 },
-    //     { id: 20, name: "User 20", points: 150 },
-    //     { id: 21, name: "User 21", points: 500 },
-    //     { id: 22, name: "User 22", points: 10 },
-    //   ];
       
     const [users, setUsers] = useState([]);
     useEffect(() => {
@@ -89,15 +65,13 @@ function Leaderboards(props) {
           <ul className="leaderboards-list">
           {usersOnCurrentPage.map((user, index) => (
             <li key={index} className="leaderboards-box">
-              {/* fix for total points */}
               {user.username} - {user.points} points
-              {/* {user.username} - {user.points.Spending} points */}
             </li>
           ))}
           </ul>
   
           <div className="pagination">
-            <button onClick={handlePrevPage}>&lt; Prev</button>
+            <button onClick={handlePrevPage} disabled={currentPage === 1}>&lt; Prev</button>
             {pageRange.map((page) => (
               <button
                 key={page}
@@ -107,7 +81,7 @@ function Leaderboards(props) {
                 {page}
               </button>
             ))}
-            <button onClick={handleNextPage}>Next &gt;</button>
+            <button onClick={handleNextPage} disabled={currentPage === totalPages}>Next &gt;</button>
           </div>
         </div>
       </>
